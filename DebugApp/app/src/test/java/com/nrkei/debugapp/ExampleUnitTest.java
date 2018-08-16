0536dd9d1cd220035d90b21c96a4e1a24fc442ae
Fred George <fredgeorge@acm.org>
2018-08-16 13:49:48 +0200

    Fred: Unit testing lesson 3.2

--- DebugApp/app/src/test/java/com/nrkei/debugapp/ExampleUnitTest.java
+++ DebugApp/app/src/test/java/com/nrkei/debugapp/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void multiply() {
+        assertEquals(12, 3 * 4);
+    }
 }
